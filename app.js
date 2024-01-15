2043d8edde085742ed49575f0fe4a4d500c7a632
Darkx23312 <130650239+Darkx-dev@users.noreply.github.com>
2024-01-15 21:51:52 +0530

    main

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-const lenis = new Lenis()
-
-// lenis.on('scroll', (e) => {
-//   console.log(e)
-// })
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
-
-raf()
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[#main, body]'),
+  smooth: true
+});
